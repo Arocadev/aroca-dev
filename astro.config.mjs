fcd95ddfe36b9c9c2b7d56604f4739cbc8c6596d
ArocaDev <aleexrc.xtv@gmail.com>
2026-07-25 14:46:32 +0200

    Arreglos V1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://aroca.dev',
+  integrations: [sitemap()]
+});
